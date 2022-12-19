44acddbff11ba8d900eaf01708e1916b8c889cb4
Cooper Garren <cgarren18@icloud.com>
2022-12-19 02:37:44 -0500

    Added tabs and other niceties

--- src/components/BracketCard.jsx
+++ src/components/BracketCard.jsx
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { loadRequest } from "../utilities/helpers";
+import LoadingIndicator from "./LoadingIndicator";
 
-const BracketCard = ({ bracketName, image }) => {
+const BracketCard = ({ bracket }) => {
+  const [image, setImage] = useState(null);
+
+  React.useEffect(() => {
+    if (bracket.artistId) {
+      getArtistImage().then((image) => {
+        setImage(image);
+      });
+    }
+  }, [bracket]);
+
+  async function getArtistImage() {
+    const url = "https://api.spotify.com/v1/artists/" + bracket.artistId;
+    console.log(url);
+    const response = await loadRequest(url);
+    return response.images[0].url;
+  }
+
   return (
-    <div className="text-center">
-      <div className="bg-white p-3 rounded-lg">
-        <img src={image} width="700px" height="310px" />
+    <button className="text-center p-3">
+      <div className="bg-white rounded-lg w-[320px] h-[320px]">
+        {image ? (
+          <img src={image} className="" />
+        ) : (
+          <LoadingIndicator loadingText="" hidden={false} />
+        )}
       </div>
-      <div>bracketName</div>
-    </div>
+      <div className="inline-flex gap-0.5">
+        <span>
+          {bracket.artistName
+            ? bracket.artistName + " (" + bracket.tracks + " tracks)"
+            : "Getting brackets..."}
+        </span>
+        {bracket.complete ? (
+          <span className="text-green-600 text-xs font-medium inline-flex items-center px-0.5 py-0.5 rounded-md">
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </span>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </button>
   );
 };
 
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,36 +1,56 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../components/Layout";
 import BracketCard from "../components/BracketCard";
-import kanyeBracketImage from "../assets/images/kanyeBracket.png";
+import Tab from "../components/Tab";
 
 // markup
 const App = () => {
   const [brackets, setBrackets] = useState([
-    { id: 1, name: "", image: null },
+    { id: 1, artistName: null, artistId: null, tracks: null, seeding: null, lastModified: null, complete: false },
   ]);
+  const [shownBrackets, setShownBrackets] = useState(brackets);
+  const [activeTab, setActiveTab] = useState(0);
+
+  useEffect(() => {
+    setShownBrackets(brackets.filter((bracket) => {
+      if (activeTab === 0) return true;
+      if (activeTab === 1) return !bracket.complete;
+      if (activeTab === 2) return bracket.complete;
+    }));
+  }, [activeTab]);
 
   useEffect(() => {
     async function getBrackets() {
       await new Promise(resolve => setTimeout(resolve, 5000)); //simulate network loading
-      setBrackets([
-        { id: 1, name: "Kanye West Bracket", image: kanyeBracketImage },
-        { id: 2, name: "Kanye West Bracket", image: kanyeBracketImage },
-        { id: 3, name: "Kanye West Bracket", image: kanyeBracketImage },
-        { id: 4, name: "Kanye West Bracket", image: kanyeBracketImage },
-        { id: 5, name: "Kanye West Bracket", image: kanyeBracketImage },
-        { id: 6, name: "Kanye West Bracket", image: kanyeBracketImage },
-      ]);
+      const loadedBrackets = [
+        { id: 1, artistName: "Kanye West", artistId: "5K4W6rqBFWDnAN6FQUkS6x", tracks: 64, seeding: "popularity", lastModified: Date.now(), complete: true },
+        { id: 2, artistName: "Ariana Grande", artistId: "66CXWjxzNUsdJxJ2JdwvnR", tracks: 32, seeding: "popularity", lastModified: Date.now(), complete: false },
+        { id: 3, artistName: "Kanye West", artistId: "5K4W6rqBFWDnAN6FQUkS6x", tracks: 8, seeding: "popularity", lastModified: Date.now(), complete: false },
+        { id: 4, artistName: "Kanye West", artistId: "5K4W6rqBFWDnAN6FQUkS6x", tracks: 128, seeding: "popularity", lastModified: Date.now(), complete: true },
+        { id: 5, artistName: "Kanye West", artistId: "5K4W6rqBFWDnAN6FQUkS6x", tracks: 16, seeding: "popularity", lastModified: Date.now(), complete: false },
+        { id: 6, artistName: "Kanye West", artistId: "5K4W6rqBFWDnAN6FQUkS6x", tracks: 32, seeding: "popularity", lastModified: Date.now(), complete: true },
+      ]
+      setBrackets(loadedBrackets);
+      setShownBrackets(loadedBrackets);
     }
     getBrackets();
   }, []);
 
   return (
     <Layout noChanges={true}>
-      <div className="">
+      <div className="text-center">
         <h1 className="text-4xl font-extrabold mb-2">My Brackets</h1>
-        <div className="flex flex-row flex-wrap justify-evenly items-center gap-5 flex-shrink">
-          {brackets.map((bracket) => (
-            <BracketCard name={bracket.name} image={bracket.image} key={bracket.id} />
+
+        <div className="mb-3">
+          <nav className="inline-flex flex-col sm:flex-row ">
+            <Tab id={0} activeTab={activeTab} setActiveTab={setActiveTab} text="All" />
+            <Tab id={1} activeTab={activeTab} setActiveTab={setActiveTab} text="In Progess" />
+            <Tab id={2} activeTab={activeTab} setActiveTab={setActiveTab} text="Completed" />
+          </nav>
+        </div>
+        <div className="flex flex-row flex-wrap justify-start items-stretch gap-5 overflow-scroll mx-5">
+          {shownBrackets.map((bracket) => (
+            <BracketCard bracket={bracket} key={bracket.id} />
           ))}
         </div>
       </div>
