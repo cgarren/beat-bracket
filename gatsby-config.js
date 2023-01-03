4a7be94cc9f29fe631c563c12fba691fe4d361de
Cooper Garren <cgarren18@icloud.com>
2023-01-03 03:33:31 -0600

    updated gatsby and added webmanifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,27 @@ module.exports = {
     title: `Beat Bracket`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-react-helmet', {
+  plugins: [{
     resolve: "gatsby-plugin-react-svg",
     options: {
       rule: {
         include: /svgs/ // Including svgs in image folder for now
       }
     }
-  }],
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: "Beat Bracket",
+      short_name: "Beat Bracket",
+      start_url: "/",
+      background_color: "#000000",
+      theme_color: "#ffffff",
+      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+      display: "standalone",
+      icon: "src/assets/images/beat_bracket_logo.png", // This path is relative to the root of the site.
+    },
+  },],
   pathPrefix: "/beat-bracket"
 }
