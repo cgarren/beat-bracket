6e2de95daddc2ef533c2afa7c854eff6ecd6d863
Cooper Garren <cgarren18@icloud.com>
2023-01-03 01:14:50 -0600

    removed auth banner

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -38,9 +38,9 @@ const Layout = ({ children, noChanges }) => {
         <title>Beat Bracket</title>
       </Helmet>
       {/* <div className="fixed w-full h-full top-0 left-0 bg-repeat bg-scroll bg-slate-900 bg-colosseum bg-blend-screen bg-cover opacity-40 -z-10"></div> */}
-      <AuthBanner show={!loggedIn} />
+      <AuthBanner show={false} />
       <NavBar loggedIn={loggedIn} noChanges={noChanges} userInfo={userInfo} />
-      <div className="">{children}</div>
+      {children}
     </main>
   );
 };
