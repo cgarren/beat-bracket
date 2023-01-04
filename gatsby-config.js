c009a8c011a2119a0fa5dfce5ef793ca6d9667c2
Cooper Garren <cgarren18@icloud.com>
2023-01-04 04:25:01 -0600

    fixed styling by bringing back postcss for tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Beat Bracket`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
+  plugins: ['gatsby-plugin-postcss', {
     resolve: "gatsby-plugin-react-svg",
     options: {
       rule: {
@@ -11,19 +11,19 @@ module.exports = {
       }
     }
   },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: "Beat Bracket",
-      short_name: "Beat Bracket",
-      start_url: "/",
-      background_color: "#000000",
-      theme_color: "#ffffff",
-      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      display: "standalone",
-      icon: "src/assets/images/beat_bracket_logo.png", // This path is relative to the root of the site.
-    },
-  },],
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Beat Bracket",
+        short_name: "Beat Bracket",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#ffffff",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/assets/images/beat_bracket_logo.png", // This path is relative to the root of the site.
+      },
+    },],
   pathPrefix: "/beat-bracket"
 }
