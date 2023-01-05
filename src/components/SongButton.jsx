c6f731cb76750099a596e4e32062a00d28195685
Cooper Garren <cgarren18@icloud.com>
2023-01-05 12:31:12 -0600

    better autoplay handling in safari

--- src/components/SongButton.jsx
+++ src/components/SongButton.jsx
@@ -127,7 +127,18 @@ const SongButton = ({
       setPaused(true);
     } else {
       try {
-        audioRef.current.play();
+        audioRef.current.play().catch((error) => {
+          console.log(error.name);
+          if (error.name === "NotAllowedError") {
+            alert(
+              "It looks like you haven't given this site permission to play audio. If you are using Safari, please enable autoplay and try again. If that doesn't work, try using Chrome instead!"
+            );
+          } else {
+            console.log(error);
+          }
+          setCurrentlyPlayingId(null);
+          setPaused(true);
+        });
         setPaused(false);
       } catch (error) {
         console.log(error);
