8340af4b971d6270296baf1d6a4c9317f1aa9b68
Cooper Garren <cgarren18@icloud.com>
2023-01-10 22:37:12 -0500

    fixed site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Beat Bracket`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.beatbracket.com`,
   },
   plugins: ['gatsby-plugin-postcss', {
     resolve: "gatsby-plugin-react-svg",
