ea533a59e98b879e5b8813fc7bf13c794568aad2
Cooper Garren <cgarren18@icloud.com>
2023-03-01 03:05:47 -0500

    abstracted modal into own component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Modal = ({ onClose, children }) => {
+  return (
+    <div
+      tabIndex="-1"
+      className="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto inset-0 h-modal h-full bg-black bg-opacity-50"
+    >
+      <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-full max-w-md h-auto">
+        <div className="relative bg-white rounded-lg shadow">
+          <button
+            type="button"
+            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center "
+            onClick={onClose}
+          >
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          <div className="p-6 text-center">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
