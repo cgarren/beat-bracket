968f629a610136527e6a3cfe5c94661f68ac7baf
Cooper Garren <cgarren18@icloud.com>
2023-04-10 00:05:10 -0400

    make the landing page more informative

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,13 @@ const App = () => {
     <>
       <Clicky />
       <main className="h-screen bg-zinc-300">
-        <nav className="absolute w-full z-50 p-2 bg-black"></nav>
+        {/* <nav className="absolute w-full z-50 p-2 bg-black"></nav> */}
         <div className="flex flex-col justify-center items-center h-full">
-          <div className="inline-flex flex-col justify-center items-center">
-            <div className="relative">
-              <LandingLetter letter={"Beat Bracket"} animation={"animate-flipy"} />
-            </div>
-            {/* <div className="mb-0.5 text-black font-bar font-bold text-xl">THE song bracket maker</div> */}
+          <div className="inline-flex flex-col justify-center items-center text-center">
+            <LandingLetter letter={"Beat Bracket"} animation={"animate-flipy"} />
+            <div className="mb-0.5 text-black font-bar font-bold text-xl">Make interactive music brackets for your favorite artists!</div>
             <span className="mt-1.5"><LoginButton /></span>
+            <p className="text-sm text-gray-500">A Spotify account is also a Beat Bracket account</p>
           </div>
         </div>
       </main>
