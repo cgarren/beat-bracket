4cffa92631e922724c797d116c82456c81c799f8
Cooper Garren <cgarren18@icloud.com>
2023-04-10 11:17:17 -0400

    removed canonical link tag

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -22,7 +22,7 @@ export const SEO = ({ title, description, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-      <link rel="canonical" href={seo.url}></link>
+      {/* <link rel="canonical" href={seo.url}></link> */}
       {/* <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
