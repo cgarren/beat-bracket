954237ee735adc114f944dd83062e4b434150fdf
Cooper Garren <cgarren18@icloud.com>
2023-04-19 00:37:12 -0400

    stopped showing footer on landing page load

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const App = () => {
       <Clicky />
       <main className="h-screen bg-gradient-radial from-zinc-100 from-40% to-zinc-400">
         {/* <nav className="absolute w-full z-50 p-2 bg-black"></nav> */}
-        <div className="flex flex-col justify-center items-center h-5/6 px-4">
+        <div className="flex flex-col justify-center items-center h-full px-4">
           <div className="inline-flex flex-col justify-center items-center text-center">
             <h1 className="inline-block mb-0.5 font-bold font-display sm:text-8xl text-7xl text-black ">Beat Bracket</h1>
             <h2 className="mb-0.5 text-black font-bar font-bold text-xl">Make interactive music brackets for your favorite artists!</h2>
@@ -20,7 +20,7 @@ const App = () => {
             {/* <p className="text-sm text-gray-600">A Spotify account is required to create and save brackets</p> */}
           </div>
         </div>
-        <Footer heightClass="h-1/6" />
+        <Footer heightClass="" />
       </main>
     </>
   )
