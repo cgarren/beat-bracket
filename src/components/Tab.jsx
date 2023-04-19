27d61f358d8d4de4e985f3b6a28cee5d82d41d0e
Cooper Garren <cgarren18@icloud.com>
2023-04-19 15:19:41 -0400

    fixed tab bottom border display

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -6,7 +6,7 @@ const Tab = ({ text, activeTab, id, setActiveTab }) => {
       title={`${text} tab`}
       className={
         "py-4 px-6 block bg-transparent hover:bg-transparent border-transparent hover:text-blue-700 hover:border-blue-700 focus:outline-none border-b-2 border-x-0 border-t-0 rounded-none font-medium " +
-        (activeTab === id ? "text-blue-700 border-blue-700" : "text-gray-600")
+        (activeTab === id ? "text-blue-700 !border-blue-700" : "text-gray-600")
       }
       onClick={() => setActiveTab(id)}
     >
