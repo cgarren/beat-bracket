f73205b0f9438607f363ada73b2f5336c3ed163c
Cooper Garren <cgarren18@icloud.com>
2023-05-09 18:36:19 -0400

    added responsiveness to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,27 @@ const Footer = ({ heightClass, loggedIn, path }) => {
       <div className="bg-black">
         <Faq loggedIn={loggedIn} path={path} />
         <div className="relative text-center">
-          <span className="inline-flex items-center text-white">
-            © Cooper Garren 2023 |&nbsp;
-            <a className="hover:underline" href="/privacy-policy">
+          <span className="inline-flex items-center text-white sm:flex-row flex-col">
+            © Cooper Garren 2023
+            <span className="hidden sm:block">&nbsp;|&nbsp;</span>
+            <a
+              className="hover:underline"
+              target="_blank"
+              href="/privacy-policy"
+            >
               Privacy Policy
             </a>
-            &nbsp;| Content from&nbsp;
-            <a href="https://spotify.com">
-              <img
-                src={spotifyLogo}
-                alt="Spotify"
-                className="h-[22px] text-white"
-              />
-            </a>
+            <span className="hidden sm:block">&nbsp;|&nbsp;</span>
+            <span className="flex">
+              Content from&nbsp;
+              <a href="https://spotify.com">
+                <img
+                  src={spotifyLogo}
+                  alt="Spotify"
+                  className="h-[22px] text-white"
+                />
+              </a>
+            </span>
           </span>
         </div>
       </div>
