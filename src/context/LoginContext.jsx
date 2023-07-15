597acffed86f17fe9b23dd0c489003d02e8c26d7
Cooper Garren <cgarren18@icloud.com>
2023-07-15 00:51:03 -0500

    Create LoginContext.jsx

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useEffect } from "react";
+import { isLoggedIn } from "../utilities/authentication";
+export const LoginContext = createContext(null);
+
+export function LoginProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+  //const [, updateState] = useState();
+  useEffect(() => {
+    // check if user is logged in when the localstorage changes
+    window.onstorage = () => {
+      setLoggedIn(isLoggedIn());
+    };
+    return () => {
+      window.onstorage = null;
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   //updateState({});
+  //   // check if user is logged in when the localstorage changes
+  //   window.onstorage = () => {
+  //     console.log("storage changed");
+  //     updateState({});
+  //   };
+
+  //   return () => {
+  //     window.onstorage = null;
+  //   };
+  // }, []);
+
+  return (
+    <LoginContext.Provider value={{ loggedIn, setLoggedIn }}>
+      {children}
+    </LoginContext.Provider>
+  );
+}
