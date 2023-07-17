f4d9b3b5ffa88c83be723a4e55190e54dd56827c
Cooper Garren <cgarren18@icloud.com>
2023-07-17 17:32:02 -0500

    Make favicon adapt to dark mode and made maskable icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,55 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "static/default-logo.png", // This path is relative to the root of the site.
+        icon: "src/assets/images/logo.svg", // This path is relative to the root of the site.
+        icons: [
+          {
+            "src": "icons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-maskable.png",
+            "sizes": "1024x1024",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
       },
     },]
 }
