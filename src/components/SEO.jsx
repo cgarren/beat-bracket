b31ffdec99410b7ef52520df81cf330e53613cbf
Cooper Garren <cgarren18@icloud.com>
2023-07-27 23:46:04 -0400

    fixed meta tag prop

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,35 +3,35 @@ import React from "react";
 import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
 export const Seo = ({ title, description, pathname, children }) => {
-  const {
-    title: defaultTitle,
-    description: defaultDescription,
-    // image,
-    siteUrl,
-  } = useSiteMetadata();
+    const {
+        title: defaultTitle,
+        description: defaultDescription,
+        // image,
+        siteUrl,
+    } = useSiteMetadata();
 
-  const seo = {
-    siteName: defaultTitle,
-    title: title ? `${title} | ${defaultTitle}` : defaultTitle,
-    pageTitle: title ? `${title}` : defaultTitle,
-    description: description || defaultDescription,
-    // image: `${siteUrl}${image}`,
-    url: `${siteUrl}${pathname || ``}`,
-  };
+    const seo = {
+        siteName: defaultTitle,
+        title: title ? `${title} | ${defaultTitle}` : defaultTitle,
+        pageTitle: title ? `${title}` : defaultTitle,
+        description: description || defaultDescription,
+        // image: `${siteUrl}${image}`,
+        url: `${siteUrl}${pathname || ``}`,
+    };
 
-  return (
-    <>
-      <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
+    return (
+        <>
+            <title>{seo.title}</title>
+            <meta name="description" content={seo.description} />
 
-      <meta itemprop="name" content={seo.pageTitle} />
-      {/* <meta itemprop="description" content={seo.description} /> */}
+            <meta itemProp="name" content={seo.pageTitle} />
+            {/* <meta itemProp="description" content={seo.description} /> */}
 
-      <meta name="og:title" content={seo.pageTitle} />
-      {/* <meta name="og:description" content={seo.description} /> */}
-      <meta name="og:site_name" content={seo.siteName} />
-      <meta name="og:type" content="website" />
-      {children}
-    </>
-  );
+            <meta name="og:title" content={seo.pageTitle} />
+            {/* <meta name="og:description" content={seo.description} /> */}
+            <meta name="og:site_name" content={seo.siteName} />
+            <meta name="og:type" content="website" />
+            {children}
+        </>
+    );
 };
