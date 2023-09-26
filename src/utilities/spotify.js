eb93b3d9679c787bb1f7fb9506c8c3f9af34152c
Cooper Garren <cgarren18@icloud.com>
2023-09-25 20:31:20 -0400

    better error handling

--- src/utilities/spotify.js
+++ src/utilities/spotify.js
@@ -80,7 +80,7 @@ export async function putRequest(url, params, data) {
 }
 
 export async function getArt(imageArray, type, getlargest = false) {
-	if (imageArray.length > 0) {
+	if (imageArray && imageArray.length > 0) {
 		if (getlargest) {
 			for (let i = 0; i < imageArray.length; i++) {
 				if (imageArray[i].url) {
