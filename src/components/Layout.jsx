b557b5c74085de6e0def6da91f914101e87c46f4
Cooper Garren <cgarren18@icloud.com>
2024-01-07 16:13:09 -0500

    fixed autoupdating cp date and some formatting

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -83,8 +83,11 @@ export default function Layout({
       <Clicky />
       {(showFooter || showNavBar || children) && (
         <div className="text-center clear-both">
-          <main className="font-sans text-black bg-gradient-radial from-zinc-200 to-zinc-300 relative text-center min-h-screen pb-[24px]">
-            {/* <div className="fixed w-full h-full top-0 left-0 bg-repeat bg-scroll bg-slate-900 bg-colosseum bg-blend-screen bg-cover opacity-40 -z-10"></div> */}
+          <main
+            className={`font-sans text-black bg-gradient-radial from-zinc-200 to-zinc-300 relative text-center min-h-screen ${
+              showFooter ? "pb-[24px]" : ""
+            }`}
+          >
             {showNavBar && <NavBar noChanges={noChanges} />}
             <LoginExpiredModal
               showModal={showLoginModal}
