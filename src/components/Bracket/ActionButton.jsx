17ed654902186b52a5e6d1e07a981fde305a923a
Cooper Garren <cgarren18@icloud.com>
2024-01-14 13:02:54 -0500

    made action button variants

--- src/components/Bracket/ActionButton.jsx
+++ src/components/Bracket/ActionButton.jsx
@@ -11,6 +11,28 @@ export default function ActionButton({
   customStyling = "",
   autoFocus = false,
 }) {
+  const buttonVariants = {
+    primary: {
+      backgroundColor: "bg-gray-800",
+      textColor: "text-white",
+      hoverBackgroundColor: "bg-gray-700",
+    },
+    secondary: {
+      backgroundColor: "bg-gray-200",
+      textColor: "text-black",
+      hoverBackgroundColor: "bg-gray-300",
+    },
+    white: {
+      backgroundColor: "bg-white",
+      textColor: "text-black",
+      hoverBackgroundColor: "bg-gray-100",
+    },
+    danger: {
+      backgroundColor: "bg-red-600",
+      textColor: "text-white",
+      hoverBackgroundColor: "bg-red-700",
+    },
+  };
   return (
     <button
       type="button"
@@ -19,11 +41,12 @@ export default function ActionButton({
       // autoFocus={autoFocus}
       className={cx(
         customStyling,
-        { "bg-gray-800 text-white hover:bg-black": variant === "primary" },
-        { "bg-gray-200 text-black hover:bg-gray-300": variant === "secondary" },
-        { "bg-white text-black hover:bg-gray-100": variant === "white" },
-        { "bg-red-600 text-white hover:bg-red-700": variant === "danger" },
-        "border-0 hover:disabled:border-gray-200 flex items-center gap-1 flex-nowrap focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50",
+        buttonVariants[variant].backgroundColor,
+        buttonVariants[variant].textColor,
+        `hover:enabled:${buttonVariants[variant].hoverBackgroundColor}`,
+        `hover:disabled:${buttonVariants[variant].backgroundColor}`,
+        `disabled:${buttonVariants[variant].textColor}`,
+        "disabled:opacity-100 disabled:cursor-not-allowed border-0 flex justify-center items-center gap-1 flex-nowrap focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50",
       )}
     >
       {icon}
--- src/components/Bracket/BracketCompleteModal.jsx
+++ src/components/Bracket/BracketCompleteModal.jsx
@@ -10,30 +10,28 @@ export default function BracketCompleteModal({ showModal, setShowModal, bracketW
     <div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <div className="flex flex-col justify-between align-middle items-center gap-0">
+          <div className="flex flex-col justify-between align-middle gap-0">
             <h1 className="text-xl font-bold">Bracket Complete!</h1>
             <BracketWinnerInfo
               bracketWinner={bracketWinner}
               showSongInfo={songSource && songSource.type === "playlist"}
             />
 
-            <div className="mt-2 flex flex-row justify-evenly">
-              <ActionButton
-                onClick={() => setShowModal(false)}
-                text="Admire my masterpiece"
-                customStyling="mr-2 bg-gray-200 hover:bg-gray-300"
-              />
+            <div className="mt-2 flex flex-row justify-around">
+              <ActionButton onClick={() => setShowModal(false)} text="Admire my masterpiece" variant="secondary" />
               <ActionButton
                 onClick={() => {
                   navigate("/my-brackets");
                 }}
                 text="Start a new bracket"
                 autoFocus
-                customStyling="bg-gray-700 text-white hover:bg-black disabled:text-gray-400 hover:disabled:bg-black"
+                variant="primary"
                 disabled={!isSaved}
               />
             </div>
-            <SaveIndicator saving={saving} isSaved={isSaved} />
+            <div className="flex flex-row justify-around">
+              <SaveIndicator saving={saving} isSaved={isSaved} />
+            </div>
           </div>
         </Modal>
       )}
