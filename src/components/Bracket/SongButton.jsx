ef2a7d2b5e103b9d64cf76c78fbb022f84cff530
Cooper Garren <cgarren18@icloud.com>
2024-01-15 18:17:26 -0500

    changed song replace icon

--- src/components/Bracket/SongButton.jsx
+++ src/components/Bracket/SongButton.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useMemo, useCallback } from "react";
 import Vibrant from "node-vibrant";
 import cx from "classnames";
+import SwapIcon from "../../assets/svgs/swapIcon.svg";
 import UndoIcon from "../../assets/svgs/undoIcon.svg";
 import PlayPauseButton from "./PlayPauseButton";
 import useBracketGeneration from "../../hooks/useBracketGeneration";
@@ -203,14 +204,14 @@ export default function SongButton({
 
   return (
     <>
-      {replacementTracks && showTrackSelector ? (
+      {replacementTracks && showTrackSelector && (
         <ReplaceTrackModal
           setShow={setShowTrackSelector}
           replacementTracks={replacementTracks}
           handleReplacement={handleReplacement}
           showSongInfo={showSongInfo}
         />
-      ) : null}
+      )}
       <div className="relative">
         {editable && !editMode && song && !disabled && col !== 0 && undoFunc && (
           <button
@@ -284,9 +285,12 @@ export default function SongButton({
               onClick={() => {
                 setShowTrackSelector(true);
               }}
-              className="border-0 p-0 w-[20px] h-[20px] hover:bg-gray-200 bg-white text-black absolute -top-2 -right-2 rounded-full z-20 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50"
+              className="border-0 p-0 flex items-center justify-center w-[20px] h-[20px] hover:bg-gray-200 bg-white text-black absolute -top-2 -right-2 rounded-full z-20 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50"
+              aria-label="Replace track"
             >
-              ✕
+              <div className="w-[16px] h-[16px]">
+                <SwapIcon />
+              </div>
             </button>
           ) : null}
           {!editMode && song && !disabled ? (
