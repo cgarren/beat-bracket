2294b26d962f9ffd741559118ba594e579bcd39a
Cooper Garren <cgarren18@icloud.com>
2024-01-23 01:45:46 -0500

    made functions into useCallbacks

--- src/components/Bracket/FillBracket.jsx
+++ src/components/Bracket/FillBracket.jsx
@@ -27,68 +27,56 @@ export default function FillBracket({
     [bracket, getBracket, setBracket],
   );
 
-  function undoChoice(id, previousIds) {
-    const triples = [
-      [id, "song", null],
-      [id, "disabled", true],
-      [id, "color", null],
-      [id, "winner", false],
-    ];
+  const undoChoice = useCallback(
+    (id, previousIds) => {
+      const triples = [
+        [id, "song", null],
+        [id, "disabled", true],
+        [id, "color", null],
+        [id, "winner", false],
+      ];
 
-    if (previousIds?.length > 0) {
-      previousIds.forEach((prevId) => {
+      if (previousIds?.length > 0) {
+        previousIds.forEach((prevId) => {
+          triples.push(
+            ...[
+              [prevId, "disabled", false],
+              [prevId, "eliminated", false],
+            ],
+          );
+        });
+      }
+      setCurrentlyPlayingId(null);
+      modifyBracket(triples);
+    },
+    [modifyBracket, setCurrentlyPlayingId],
+  );
+
+  const makeChoice = useCallback(
+    (id, opponentId, nextId, song, color) => {
+      const triples = [
+        [id, "disabled", true],
+        [opponentId, "disabled", true],
+        [opponentId, "eliminated", true],
+      ];
+      // eliminatePrevious(opponentId);
+      if (nextId) {
         triples.push(
           ...[
-            [prevId, "disabled", false],
-            [prevId, "eliminated", false],
+            [nextId, "song", song],
+            [nextId, "disabled", false],
+            [nextId, "color", color, true],
           ],
         );
-      });
-    }
-    // const triples = [
-    //   [id, "disabled", false],
-    //   [opponentId, "disabled", false],
-    //   [opponentId, "eliminated", false],
-    // ];
-    // undoEliminatePrevious(opponentId);
-    // if (nextId) {
-    //   triples.push(
-    //     ...[
-    //       [nextId, "song", null],
-    //       [nextId, "disabled", true],
-    //       [nextId, "color", null],
-    //     ],
-    //   );
-    // } else {
-    //   triples.push(...[[id, "winner", false]]);
-    // }
-    setCurrentlyPlayingId(null);
-    modifyBracket(triples);
-  }
-
-  function makeChoice(id, opponentId, nextId, song, color) {
-    const triples = [
-      [id, "disabled", true],
-      [opponentId, "disabled", true],
-      [opponentId, "eliminated", true],
-    ];
-    // eliminatePrevious(opponentId);
-    if (nextId) {
-      triples.push(
-        ...[
-          [nextId, "song", song],
-          [nextId, "disabled", false],
-          [nextId, "color", color, true],
-          // [nextId, "undoFunc", undoChoice],
-        ],
-      );
-    } else {
-      console.log(`Winner is ${song.name}`);
-      triples.push(...[[id, "winner", true]]);
-    }
-    modifyBracket(triples);
-    setCurrentlyPlayingId(null);
-  }
+      } else {
+        console.log(`Winner is ${song.name}`);
+        triples.push(...[[id, "winner", true]]);
+      }
+      modifyBracket(triples);
+      setCurrentlyPlayingId(null);
+    },
+    [modifyBracket, setCurrentlyPlayingId],
+  );
 
   return (
     <Bracket
