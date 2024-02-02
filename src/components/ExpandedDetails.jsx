8c9450adc2740e7126a4fb39bca3624f451cbe51
Cooper Garren <cgarren18@icloud.com>
2024-02-02 00:58:40 -0500

    Update ExpandedDetails.jsx

--- src/components/ExpandedDetails.jsx
+++ src/components/ExpandedDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ExpandedDetails({ question, answer }) {
+export default function ExpandedDetails({ question, answer, onClickHandler }) {
   return (
     // <div className="mb-5">
     //   <h2 className="font-bold text-xl text-white">{question}</h2>
@@ -8,7 +8,12 @@ export default function ExpandedDetails({ question, answer }) {
     // </div>
     <div className="w-fit mx-auto">
       <details className="rounded-lg open:bg-white open:p-3 open:ring-black/5 open:ring-1 open:shadow-lg">
-        <summary className="text-sm leading-6 text-black font-semibold select-none cursor-pointer">{question}</summary>
+        <summary
+          onClick={onClickHandler}
+          className="text-sm leading-6 text-black font-semibold select-none cursor-pointer"
+        >
+          {question}
+        </summary>
         <div className="text-sm leading-6 text-black">
           <div>{answer}</div>
         </div>
