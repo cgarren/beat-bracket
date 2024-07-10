fb5d57999f0a981510076954078ba08cc4b8525c
Cooper Garren <cgarren18@icloud.com>
2024-07-09 23:43:15 -0400

    reworked solution

--- src/pages/user/[userId]/bracket/[id]/index.jsx
+++ src/pages/user/[userId]/bracket/[id]/index.jsx
@@ -35,7 +35,7 @@ export default function App({ params, location }) {
   const { getNumberOfColumns } = useBracketGeneration();
   const { share } = useShareBracket(location.href);
 
-  const { data: { data: ownerInfo } = { data: {} } } = useUserInfo(params.userId) || {};
+  const { data: ownerInfo = {} } = useUserInfo(params.userId)?.data || {};
 
   const owner = useMemo(
     () => ({ name: ownerInfo?.display_name, id: params.userId }),
